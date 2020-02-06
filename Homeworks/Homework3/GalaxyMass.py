45dcae7a9580f8ccd241464d2f693e7b75454a22
barraganma <barraganma@email.arizona.edu>
2020-02-06 10:45:52 -0700

    updating GalaxyMass.py

--- Homeworks/Homework3/GalaxyMass.py
+++ Homeworks/Homework3/GalaxyMass.py
@@ -1,6 +1,6 @@
 import numpy as np
 import astropy.units as u
-from astropy.table import QTable, Table, Column
+from astropy.table import Table
 from ReadFile import Read
 
 def ComponentMass(filename,ptype):
@@ -29,48 +29,47 @@ def ComponentMass(filename,ptype):
 
 # Store Names
 name = ['MW','M31','M33','Local Group']
-
+    
 # Store total component masses of MW
-MWhalo = ComponentMass('MW_000.txt',1)
-MWdisk = ComponentMass('MW_000.txt',2)
-MWbulge = ComponentMass('MW_000.txt',3)
-MWtot = np.around(MWhalo + MWdisk + MWbulge,3)
-MWstellar = MWdisk + MWbulge
-MWfbar = np.around(MWstellar/MWtot,3)
+MWhalo =     ComponentMass('MW_000.txt',1)
+MWdisk =     ComponentMass('MW_000.txt',2)
+MWbulge =    ComponentMass('MW_000.txt',3)
+MWtot =      np.around(MWhalo + MWdisk + MWbulge,3)
+MWstellar =  MWdisk + MWbulge
+MWfbar =     np.around(MWstellar/MWtot,3)
 
 # Store total component masses of M31
-M31halo = ComponentMass('M31_000.txt',1)
-M31disk = ComponentMass('M31_000.txt',2)
-M31bulge = ComponentMass('M31_000.txt',3)
-M31tot = np.around(M31halo + M31disk + M31bulge,3)
+M31halo =    ComponentMass('M31_000.txt',1)
+M31disk =    ComponentMass('M31_000.txt',2)
+M31bulge =   ComponentMass('M31_000.txt',3)
+M31tot =     np.around(M31halo + M31disk + M31bulge,3)
 M31stellar = M31disk + M31bulge
-M31fbar = np.around(M31stellar/M31tot,3)
+M31fbar =    np.around(M31stellar/M31tot,3)
 
 # Store total component masses of M33 (no bulge)
-M33halo = ComponentMass('M33_000.txt',1)
-M33disk = ComponentMass('M33_000.txt',2)
-M33bulge = 0
-M33tot = np.around(M33halo + M33disk,3)
+M33halo =    ComponentMass('M33_000.txt',1)
+M33disk =    ComponentMass('M33_000.txt',2)
+M33bulge =   0
+M33tot =     np.around(M33halo + M33disk,3)
 M33stellar = M33disk + M33bulge
-M33fbar = np.around(M33stellar/M33tot,3)
+M33fbar =    np.around(M33stellar/M33tot,3)
 
 # Store total mass components of the Local Group
-localtot = np.around(MWtot + M31tot + M33tot,3)
+localtot =   np.around(MWtot + M31tot + M33tot,3)
 totstellar = MWstellar + M31stellar + M33stellar
-localfbar = np.around(totstellar/localtot,3)
+localfbar =  np.around(totstellar/localtot,3)
 
 # Store halo, disk, and bulge masses seperately in order. Used for Table
-halo = np.array([MWhalo,M31halo,M33halo,0])
-disk = np.array([MWdisk,M31disk,M33disk,0])
-bulge = np.array([MWbulge,M31bulge,M33bulge,0])
-tot = np.array([MWtot,M31tot,M33tot,localtot])
-fbar = np.array([MWfbar,M31fbar,M33fbar,localfbar])
-
-localmass = np.around(MWtot + M31tot + M33tot,3)
-totstellar = MWstellar + M31stellar + M33stellar
-localfbar = np.around(totstellar/tot,3)
+halo =       np.array([MWhalo, M31halo, M33halo, 0])
+disk =       np.array([MWdisk, M31disk, M33disk, 0])
+bulge =      np.array([MWbulge,M31bulge,M33bulge,0])
+tot =        np.array([MWtot,  M31tot,  M33tot,  localtot])
+fbar =       np.array([MWfbar, M31fbar, M33fbar, localfbar])
 
 # Make a table with name, halo, disk, bulge elements
 t = Table([name,halo,disk,bulge,tot,fbar], names=('Galaxy Name','Halo Mass (10^12 Msun)','Disk Mass (10^12 Msun)','Bulge Mass (10^12 Msun)','Total Mass (10^12 Msun)','Baryon Fraction'))
 
+# Show table
 print(t)
+
+
